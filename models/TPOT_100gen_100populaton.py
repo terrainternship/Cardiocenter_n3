f069b447f23bf111f95434576846f7269183bb14
lxndrlnk <130960160+lxndrlnk@users.noreply.github.com>
2023-11-08 22:47:50 +0300

    Add files via upload

---
+++ models/TPOT_100gen_100populaton.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import MaxAbsScaler, StandardScaler
+from tpot.builtins import ZeroCount
+from tpot.export_utils import set_param_recursive
+
+# NOTE: Make sure that the outcome column is labeled 'target' in the data file
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = tpot_data.drop('target', axis=1)
+training_features, testing_features, training_target, testing_target = \
+            train_test_split(features, tpot_data['target'], random_state=0)
+
+# Average CV score on the training set was: -0.5129832686388066
+exported_pipeline = make_pipeline(
+    StandardScaler(),
+    MaxAbsScaler(),
+    ZeroCount(),
+    VarianceThreshold(threshold=0.05),
+    ExtraTreesRegressor(bootstrap=False, max_features=0.55, min_samples_leaf=1, min_samples_split=2, n_estimators=100)
+)
+# Fix random state for all the steps in exported pipeline
+set_param_recursive(exported_pipeline.steps, 'random_state', 0)
+
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
